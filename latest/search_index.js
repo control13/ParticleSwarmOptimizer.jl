57d949f345e492cbb19b0638e98b5cd24c0880b4
autodocs <autodocs>
2018-07-15 19:19:48 +0000

    build based on d194d38

---
+++ latest/search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#ParticleSwarmOptimizer.jl-1",
+    "page": "Home",
+    "title": "ParticleSwarmOptimizer.jl",
+    "category": "section",
+    "text": "The ParticleSwarmOptimizer (PSO) is an optimization algorithm based on the idea of bird flocking or fish schooling. The PSO can also be used for nonlinear and noncontinous functions.(Image: PSO-Example)"
+},
+
+]}
